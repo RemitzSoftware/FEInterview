053b6a2bdf08d7be4d1046f04189a67ad2fd6d5b
juandeto <juandeto10@gmail.com>
2024-09-12 18:55:56 -0300

    make rows clickable and trigger query to GET client details

--- remitz-client/src/App.tsx
+++ remitz-client/src/App.tsx
@@ -11,13 +11,17 @@ mock.onGet('/reports/clients').reply(200, {
   data: clientsData,
 });
 
+mock.onGet(/\/reports\/clients\/.*/).reply(200, {
+  data: clientsData[0],
+});
+
 function App() {
   const [clients, setClients] = useState<ClientsApi[]>([]);
+  const [clientsDetails, setClientDetails] = useState<ClientsApi>();
 
   useEffect(() => {
     async function getClientsTableData() {
       await axios.get('/reports/clients').then(function (response) {
-        console.log(response.data);
         setClients(response.data.data);
       });
     }
@@ -25,10 +29,24 @@ function App() {
     getClientsTableData();
   }, []);
 
+  async function getClientById(clientId: string) {
+    await axios.get(`/reports/clients/${clientId}`).then((res) => {
+      console.log('res; ', res.data.data);
+      setClientDetails(res.data.data);
+    });
+  }
+
+  function handleClickOnRow(clientId: string) {
+    getClientById(clientId);
+  }
+
   return (
     <div className="container">
       <h1 className="text-gray-600">Remitz interview</h1>
       <div className="container m-4 my-8">
+        <div className="p-8">
+          <div className="flex w-full">{JSON.stringify(clientsDetails)}</div>
+        </div>
         <table className="table-auto w-full bg-white border border-gray-300">
           <thead>
             <tr>
@@ -45,8 +63,11 @@ function App() {
           <tbody>
             {clients?.map((client, index) => (
               <tr
+                onClick={() => handleClickOnRow(client.client.id)}
                 key={client.client.id}
-                className={index % 2 !== 0 ? 'bg-gray-200' : 'white'}
+                className={` cursor-pointer hover:bg-gray-500 ${
+                  index % 2 !== 0 ? 'bg-gray-200' : 'white'
+                }`}
               >
                 <td className="border border-gray-300 px-4 py-2">
                   {client.client.name}
