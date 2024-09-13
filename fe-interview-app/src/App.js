f0b2af9b79c9520a52a6e31755656c565cc984ef
lcarnevali <lcarnevali@apptap.com>
2024-09-13 18:57:19 -0300

    feat: created ClientsReport component to show clients from external api

---
+++ fe-interview-app/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import ClientsReport from './components/ClientsReport';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <ClientsReport />
+      </header>
+    </div>
+  );
+}
+
+export default App;
